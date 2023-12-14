32a4e943f6478039a20b34e29d257e5e580fd44c
akash <akashvpm440@gmail.com>
2023-12-14 23:46:40 +0530

    some upgrades

--- password-generator.py
+++ password-generator.py
@@ -5,7 +5,17 @@ letters = string.ascii_letters
 numbers = string.digits
 symbols = string.punctuation 
 
-all_char = letters + numbers + symbols 
+all_char =[]
 
-for i in range (0,13):
+for i in range (0,5):
+      char = random.choice(letters)
+      all_char +=char
+for i in range (0,4):
+      num = random.choice(numbers)
+      all_char += num
+for i in range (0,3):
+      sym = random.choice(symbols)
+      all_char += sym
+print (all_char)
+for i in range(0,10):
       print(random.choice(all_char),end='')
