2bd39c389727359db8d0804628b2013e02dca2a6
akash <akashvpm440@gmail.com>
2023-12-16 12:45:06 +0530

    upgrades

--- word-guesser-game.py
+++ word-guesser-game.py
@@ -3,11 +3,11 @@ import random
 #choice the word randomly
 
 heart = ["❤️","❤️","❤️","❤️","❤️","❤️"]
-words = ["Apple", "Orange","Mango", "Onion"]
+words = ["apple", "orange","mango", "onion"]
 chosen_word = random.choice(words)
-# print (chosen_word)
 
-# give the hint for the player
+
+# give to the hint for the player
 
 x = words.index(chosen_word)
 
@@ -16,21 +16,19 @@ hint = ["I am a fruit, red in color","I am a fruit, I have 11 brothers",
 
 print("\n Hint is ",hint[x])
 
-#start the game 
-
-
-
 # get input from user
 
 choice = list(chosen_word)
+word = []
 
-while True:
+while (len(choice) != 0 and len(heart) != 0):
       print("\n")
+      
       for i in range(len(choice)):
             print("'_'",end='')
 
       print("\n your have",heart)
-      player_choice = input("enter the letter: ")
+      player_choice = str(input("enter the letter: "))
 
       if player_choice not in choice:
             print("oops! you are wrong")
@@ -39,14 +37,23 @@ while True:
       if player_choice in choice:
             print("you are correct, way to go")
             choice.remove(player_choice)
+            word.append(player_choice) 
+
+      print(word,end='')
 
       if (len(choice) == 0):
-            print("you won")
-            exit()
-
+            print("\n you won")
+            
       if (len(heart) == 0):
-            print("you loose")
-            exit()
+            print("\n you loose")
+            
+      
+print("\n The chosen word is ", chosen_word)
+
+
+
+
+
 
 
 
