2373d2187f6254614095af720168875ece4a5448
akash <akashvpm440@gmail.com>
2023-12-27 01:04:19 +0530

    Virtual coffee machine

---
+++ Virtual coffee machine.py
@@ -0,0 +1,12 @@
+
+price_tag = {
+     "coffee" : 50,
+      "tea" : 25,
+      "latte" : 100,
+      "mocha" : 100 
+      }
+
+
+avaliable_items = ("Coffee","Tea","Latte","Mocha")
+
+customer_requirement = str(input(f"What would you like to have {avaliable_items}"))
