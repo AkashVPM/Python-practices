236ebb637e7eba39a19a861821d5f9c956decd1d
akash <akashvpm440@gmail.com>
2024-01-06 22:02:58 +0530

    quiz_game

--- Quiz_gane.py
+++ Quiz_gane.py
@@ -4,7 +4,10 @@ questions = [{"ques":"What is a correct syntax to output 'Hello World' in Python
 {"ques":"How do you insert 'COMMENTS' in Python code?","answer":"A"}]
 
 answer = [["A: p ('Hello World')","B: print 'Hello World'","C: printf 'Hello World'","D: print ('Hello World')"],
-["A: # command","B: // command","C:-- command"]]
+["A: # command","B: // command","C: -- command"]]
 
 for question in range(len(questions)):
       print(questions[question]["ques"])
+
+for i in answer[question]:
+      print(i)
