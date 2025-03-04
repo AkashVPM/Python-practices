1c77d7c30d857494fd52cbd8ad0a5302b57c4a06
akash <akashvpm440@gmail.com>
2025-03-05 00:26:40 +0530

    Extract_vertex_lines

---
+++ Python_Altair/Find_Area_normal.py
@@ -0,0 +1,12 @@
+
+# open file text file
+file = open(r"Actuator_Body_txt_file.txt","r")
+
+# Read each line
+Lines = file.readlines()
+
+#find ward vertex
+for line in Lines:
+      if "vertex" in line:
+            print(line)
+
